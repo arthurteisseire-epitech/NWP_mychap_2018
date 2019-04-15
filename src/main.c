700f55a0650b536e84f08101c1baa034ef3d1f90
arthur <arthur.teisseire@epitech.eu>
2019-04-15 15:02:22 +0200

    Use iphdr struct instead of unsigned char buffer

--- src/main.c
+++ src/main.c
@@ -6,18 +6,23 @@
 */
 
 #include <unistd.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
 
-void ipv4_header()
+struct iphdr ipv4_header(unsigned short data_len)
 {
-    unsigned char header[20];
+    struct iphdr header = {0};
 
-    header[0] = 0x45;
-    header[1] = 0b00011110;
-    write(1, header, 2);
+    header.version = 4;
+    header.ihl = 5;
+    header.tot_len = data_len + header.ihl * 4;
+    return header;
 }
 
 int main(void)
 {
-    ipv4_header();
+    struct iphdr ip_header = ipv4_header(0);
+
+    write(1, &ip_header, sizeof(ip_header));
     return 0;
 }
