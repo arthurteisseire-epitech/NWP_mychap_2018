828b161cfd42835feae741ef66be2a4bf7e5de29
arthur <arthur.teisseire@epitech.eu>
2019-04-15 13:16:29 +0200

    Add version and hlen in ipv4 header

--- src/main.c
+++ src/main.c
@@ -5,7 +5,18 @@
 ** main.c
 */
 
+#include <unistd.h>
+
+void ipv4_header()
+{
+    unsigned char header[20];
+
+    header[0] = (4u << 4u) | 5u;
+    write(1, header, 1);
+}
+
 int main(void)
 {
+    ipv4_header();
     return 0;
 }
