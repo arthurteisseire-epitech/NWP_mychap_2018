ec0afb6e9b2dc692ba15b2d86fdcf69b6059b9b1
arthur <arthur.teisseire@epitech.eu>
2019-04-16 14:43:30 +0200

    Add create_udp_header

--- src/main.c
+++ src/main.c
@@ -16,11 +16,11 @@ unsigned short csum(unsigned short *buf, int nwords)
 {
     unsigned long sum;
 
-    for(sum=0; nwords>0; nwords--)
+    for (sum = 0; nwords > 0; --nwords)
         sum += *buf++;
     sum = (sum >> 16u) + (sum & 0xffffu);
     sum += (sum >> 16u);
-    return (unsigned short)~sum;
+    return ~sum;
 }
 
 struct iphdr ipv4_header(unsigned short data_len, char *ip_s, char *ip_d)
@@ -35,16 +35,31 @@ struct iphdr ipv4_header(unsigned short data_len, char *ip_s, char *ip_d)
     header.frag_off = 0;
     header.ttl = 64;
     header.protocol = IPPROTO_UDP;
-    header.check = csum((unsigned short *)&header, header.tot_len);
     header.saddr = inet_addr(ip_s);
     header.daddr = inet_addr(ip_d);
+    header.check = csum((unsigned short *)&header, header.tot_len);
+    return header;
+}
+
+struct udphdr create_udp_header(void)
+{
+    struct udphdr header;
+
+    header.uh_sport = htons(2001);
+    header.uh_dport = htons(2002);
+    header.len = htons(sizeof(struct udphdr));
     return header;
 }
 
 int main(void)
 {
     struct iphdr ip_header = ipv4_header(0, "127.0.0.1", "127.0.0.1");
+    struct udphdr udp_header = create_udp_header();
+    int fd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+    int optval = 1;
 
+    setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &optval, sizeof(int));
     write(1, &ip_header, sizeof(ip_header));
+    write(1, &udp_header, sizeof(udp_header));
     return 0;
 }
