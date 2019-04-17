fffba91bb6b82a1c682a62aba1f7cd14de6acea7
arthur <arthur.teisseire@epitech.eu>
2019-04-17 16:35:37 +0200

    Fix random bytes and sha 256

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@
 #include <openssl/sha.h>
 #include "chap.h"
 
-char *receive(int fd, chap_t *chap)
+static char *receive(int fd, chap_t *chap)
 {
     struct udphdr *udp;
     char *buffer = calloc(1, 4096);
@@ -30,19 +30,19 @@ char *receive(int fd, chap_t *chap)
         }
         udp = (void *)buffer + sizeof(struct iphdr);
     } while (ntohs(udp->uh_sport) != ntohs(chap->packet->udp.uh_dport));
-    return buffer;
+    return buffer + sizeof(packet_t);
 }
 
-static char *concat(const char *left, size_t len, const char *right)
+static char *concat(const char *left, const char *right)
 {
-    char *res = calloc(1, len + strlen(right) + 1);
+    char *res = calloc(1, 10 + strlen(right) + 1);
 
-    memcpy(res, left, len);
-    memcpy(&res[len], right, strlen(right));
+    strncpy(res, left, 10);
+    strncpy(res + 10, right, strlen(right));
     return (res);
 }
 
-char *sha256(const char *str, size_t nb_bytes)
+static char *sha256(const char *str, size_t nb_bytes)
 {
     unsigned char hash[SHA256_DIGEST_LENGTH] = {0};
     SHA256_CTX sha256;
@@ -52,7 +52,7 @@ char *sha256(const char *str, size_t nb_bytes)
         sizeof(char) * ((SHA256_DIGEST_LENGTH * 2) + 1))))
         return (NULL);
     SHA256_Init(&sha256);
-    SHA256_Update(&sha256, str, nb_bytes - sizeof(packet_t));
+    SHA256_Update(&sha256, str, nb_bytes);
     SHA256_Final(hash, &sha256);
     for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
         sprintf(output_buffer + (i * 2), "%02x", hash[i]);
@@ -62,7 +62,7 @@ char *sha256(const char *str, size_t nb_bytes)
 
 static void send_password(int fd, chap_t *chap, const char *random_bytes)
 {
-    char *str = concat(random_bytes, 10, chap->password);
+    char *str = concat(random_bytes, chap->password);
     char *hash = sha256(str, 10 + strlen(chap->password));
 
     init_packet(chap, hash);
@@ -82,7 +82,11 @@ int main(int ac, char *av[])
     send_to(&chap, fd, size);
     rec = receive(fd, &chap);
     send_password(fd, &chap, rec);
+    fd = init_socket();
     rec = receive(fd, &chap);
-    printf("%s\n", rec + sizeof(packet_t));
+    if (strncmp(rec, "KO", 2) == 0)
+        printf("KO\n");
+    else
+        printf("Secret: '%s'\n", rec);
     return 0;
 }
